ac3ffe0cc6e6e4ad9f64b9a2ec99a8b924906055
zhamilka1 <zhamilistono@gmail.com>
2022-09-20 12:07:42 +0300

    list6

--- tasks/6.6.py
+++ tasks/6.6.py
@@ -1,8 +1,6 @@
 list1 = [int(s) for s in input().split()]
 list2 = [int(s) for s in input().split()]
-counter = 0
 for element1 in list1:
     for element2 in list2:
         if(element1 == element2):
-            counter +=1
-print(counter)
+            print(f'{element1} ', end="")
