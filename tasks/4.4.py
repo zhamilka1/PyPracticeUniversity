8ee837c9aa5be34c75882b3ddc5359a941ad6801
zhamilka1 <zhamilistono@gmail.com>
2022-09-20 10:53:04 +0300

    if2

---
+++ tasks/4.4.py
@@ -0,0 +1,5 @@
+z = int(input())
+if (z // 100 >=1):
+  print("да")
+else :
+  print("нет")
---
+++ tasks/4.5.py
@@ -0,0 +1,6 @@
+z = int(input())
+x = int(input())
+if (z<0) and (x<0):
+  print("нет")
+else:
+  print("да")
