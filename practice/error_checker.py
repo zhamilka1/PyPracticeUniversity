92b4de0ce5c59198f730ca6195d1f93b3a2dc77a
zhamka1 <zhamilistono@gmail.com>
2022-10-18 12:23:38 +0300

    18.10.2

---
+++ practice/error_checker.py
@@ -0,0 +1,2 @@
+def check_errors(filename):
+    file = open(filename)
--- tasks/geo_fucntions.py
+++ tasks/geo_fucntions.py
@@ -1,6 +1,6 @@
 from math import floor
 from math import pi
-
+from random import randint
 def deg_to_gms(degrees):
     full_degrees = floor(degrees)
     ost = (degrees - full_degrees) *60
@@ -19,6 +19,19 @@ def deg_to_rad(deg):
 
 def rad_to_deg(rad):
     return (rad*180)/pi
+
+
+def make_deg():
+    return (randint(0*100000, 360*100000) / 100000)
+def create_list(*args, **kwargs):
+    points_list = []
+    for key, value in kwargs.items():
+        points_list.append(str(f"{key} = {deg_to_gms(value)}"))
+    for i in range(0,len(args)):
+        points_list.append(str(f"Point {i+1} : {deg_to_gms(args[i])}"))
+    return points_list
+
 print(deg_to_gms(36.97))
 print(gms_to_deg(36, 58, 0.2))
 print(rad_to_deg(deg_to_rad(163)))
+print(create_list(make_deg(), make_deg(), make_deg(), make_deg(), make_deg(), make_deg(), zpoint=make_deg(), pole=make_deg(), kpoint=make_deg()))
