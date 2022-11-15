81c867e433900dddc8e9b9a09b26bd9630fc3d33
zhamka1 <zhamilistono@gmail.com>
2022-11-15 12:18:34 +0300

    game.path.added

--- practice/error_checker.py
+++ practice/error_checker.py
@@ -1,3 +1,23 @@
 def get_list(filename = "error_journal.txt"):
-    file = open(filename, "r")
+    try:
+        file = open(filename, "r")
+        iterator = int(file.readline())
+        numbers = []
+        for i in range(0,iterator):
+            numbers.append(int(file.readline().replace('\n', '')))
+        print(numbers)
 
+    except FileNotFoundError:
+        print('File doesnt exist')
+    except ValueError:
+        print('File is empty or have uncorrect iterations counter')
+    finally:
+        try:
+            file.close()
+        except UnboundLocalError:
+            print()
+
+get_list()
+
+
+print(z)
