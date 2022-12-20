0349d4bd69a158e2acafbc7c267363e3646594df
zhamka1 <zhamilistono@gmail>
2022-12-20 08:04:21 +0300

    9pr

--- practice/os/opersys.py
+++ practice/os/opersys.py
@@ -4,90 +4,73 @@ from docx2pdf import convert
 from PIL import Image
 
 def showMenu():
-    print("Текущая директория:" + os.getcwd())
-    print("0:Сменить рабочий каталог")
-    print("1:Преобразовать PDF в DOCX")
-    print("2:Преобразовать DOCX в PDF")
-    print("3:Произвести сжатие изображений")
-    print("4:Удалить группу файлов")
-    print("5:Выход")
+    print(os.getcwd())
+    print("0:Замена пути к рабочему каталогу")
+    print("1:Изменить файл в формате PDF в DOCX")
+    print("2:Изменить файл в формате в PDF")
+    print("3:Ужать качество изображения")
+    print("4:Удалить группу файлов по подстроке")
+    print("5:Выход из программы")
 
-def showFilesAndChooseFile():
-    filesInDirectory = os.listdir(os.getcwd())
-    for i in range(0,len(filesInDirectory)):
-        print(f"{i} : {filesInDirectory[i]}")
+def showFilesAndMakeChoise():
+    files = os.listdir(os.getcwd())
+    for i in range(0,len(files)):
+        print(f"{i} : {files[i]}")
     choise = int(input("выберите файл"))
-    return filesInDirectory[choise]
-def play():
-    showMenu()
-    choise = int(input("Введите номер"))
+    return files[choise]
+def main():
     while choise != 5:
-        if choise == 0:
-            print(changeDir())
-        elif choise == 1:
-            print(convertPDFtoDOCX())
-        elif choise == 2:
-            print(convertDOCXtoPDF())
-        elif choise == 3:
-            print(changeResolution())
-        elif choise == 4:
-            print(deleteGroupOfFiles())
-        else:
-            break
         showMenu()
         choise = int(input("Введите номер"))
-
-def changeDir():
-    newdirname = input("Введите название директории")
-    if(os.path.exists(newdirname)):
-        if(os.path.isdir(newdirname)):
-            os.chdir(newdirname)
-    return "вы перешли в директорию:" + newdirname
-
-def convertPDFtoDOCX():
-    pdfFile = showFilesAndChooseFile()
-    wordFile = pdfFile.replace('.pdf', '.docx')
-    cv = Converter(pdfFile)
-    cv.convert(wordFile)
-    cv.close()
-
-def convertDOCXtoPDF():
-    docxFile = showFilesAndChooseFile()
-    convert(docxFile)
-
-def changeResolution():
-    imagePath = showFilesAndChooseFile()
-    with Image.open(imagePath) as img:
-        print(img.format, img.size, img.format_description)
-        if img.mode != "RGB":
-            img = img.convert("RGB")
-        img.save(imagePath, "JPEG", optimize=True, quality=1)
-
-
-def deleteGroupOfFiles():
-    filesInDirectory = os.listdir(os.getcwd())
-    patternType = int(input("Введите тип паттерна: 0-префикс 1-постфикс 2-подстрока 3-расширение"))
-    if patternType == 0:
-        substr = input('Введите префикс')
-        for i in range(0, len(filesInDirectory)):
-            if filesInDirectory[i].lower().startswith(substr.lower()):
-                path = f"{os.getcwd()}\{filesInDirectory[i]}"
-                os.remove(path)
-    elif patternType == 1:
-        substr = input('Введите постфикс')
-        for i in range(0, len(filesInDirectory)):
-            if filesInDirectory[i].lower().endswith(substr.lower()):
-                path = f"{os.getcwd()}\{filesInDirectory[i]}"
-                os.remove(path)
-    elif patternType == 2:
-        substr = input('Введите подстроку')
-        for i in range(0, len(filesInDirectory)):
-            if substr.lower() in filesInDirectory[i].lower():
-                path = f"{os.getcwd()}\{filesInDirectory[i]}"
-                os.remove(path)
-    elif patternType == 3:
-        substr = input('Введите расширение')
-        for i in range(0, len(filesInDirectory)):
-            if ('.'+substr.lower()) in filesInDirectory[i].lower():
-                path = f"{os.getcwd()}\{filesInDirectory[i]}"
-                os.remove(path)
+        if choise == 0:
+            newdirname = input("Введите название директории")
+            if (os.path.exists(newdirname)):
+                if (os.path.isdir(newdirname)):
+                    os.chdir(newdirname)
+            print("вы перешли в директорию:" + newdirname)
+        elif choise == 1:
+            pdfFile = showFilesAndMakeChoise()
+            wordFile = pdfFile.replace('.pdf', '.docx')
+            cv = Converter(pdfFile)
+            cv.convert(wordFile)
+            cv.close()
+        elif choise == 2:
+            docxFile = showFilesAndMakeChoise()
+            convert(docxFile)
+        elif choise == 3:
+            imagePath = showFilesAndMakeChoise()
+            quality = int(input("Введите качество от 0 до 100%"))
+            with Image.open(imagePath) as img:
+                print(img.format, img.size)
+                if img.mode != "RGB":
+                    img = img.convert("RGB")
+                img.save(imagePath, "JPEG", optimize=True, quality=quality)
+        elif choise == 4:
+            files = os.listdir(os.getcwd())
+            searchType = int(input("Введите тип паттерна: 0-префикс 1-постфикс 2-подстрока 3-расширение"))
+            if searchType == 0:
+                substr = input('Введите префикс')
+                for file in files:
+                    if file.lower().startswith(substr.lower()):
+                        path = f"{os.getcwd()}\{file}"
+                        os.remove(path)
+            elif searchType == 1:
+                substr = input('Введите постфикс')
+                for file in files:
+                    if file.lower().endswith(substr.lower()):
+                        path = f"{os.getcwd()}\{file}"
+                        os.remove(path)
+            elif searchType == 2:
+                substr = input('Введите подстроку')
+                for file in files:
+                    if substr.lower() in file.lower():
+                        path = f"{os.getcwd()}\{file}"
+                        os.remove(path)
+            elif searchType == 3:
+                substr = input('Введите расширение')
+                for file in files:
+                    if ('.' + substr.lower()) in file.lower():
+                        path = f"{os.getcwd()}\{file}"
+                        os.remove(path)
+        else:
+            break
